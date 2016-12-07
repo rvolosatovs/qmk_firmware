800dae952b984e184cb2f46d78977f8fa6d01c10
Roman Volosatovs <rvolosatovs@riseup.net>
2016-12-07 12:35:32 +0100

    planck config: RGBLIGHT_TIMER -> RGBLIGHT_ANIMATIONS

--- keyboards/planck/config.h
+++ keyboards/planck/config.h
@@ -48,7 +48,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define UNUSED_PINS
 
 #define RGB_DI_PIN D5     // The pin your RGB strip is wired to
-#define RGBLIGHT_TIMER    // Require for fancier stuff (not compatible with audio)
+#define RGBLIGHT_ANIMATIONS     // Require for fancier stuff (not compatible with audio)
 #define RGBLED_NUM 12     // Number of LEDs
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
