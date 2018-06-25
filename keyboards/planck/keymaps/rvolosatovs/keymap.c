8872af46582e36e5bdcaa94124ed6896d408cdaa
Roman Volosatovs <rvolosatovs@riseup.net>
2018-06-25 17:12:08 +0200

    planck,preonic: Add PSCR

--- keyboards/planck/keymaps/rvolosatovs/keymap.c
+++ keyboards/planck/keymaps/rvolosatovs/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_planck_grid( \
   _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL , \
   _______, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT, _______, KC_CLCK, \
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______, \
+  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, KC_PSCR, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______  \
 )
 };
