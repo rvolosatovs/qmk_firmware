0861c3db126e49a9e0616ea1a99e1a4f24325fdf
Roman Volosatovs <rvolosatovs@riseup.net>
2018-07-12 07:48:20 +0200

    planck,preonic: Revamp

--- keyboards/planck/keymaps/rvolosatovs/keymap.c
+++ keyboards/planck/keymaps/rvolosatovs/keymap.c
@@ -42,16 +42,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_LOWER] = LAYOUT_planck_grid( \
-  KC_TILD,        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,         \
-  KC_SPC,         _______, _______, _______, _______, _______,  KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, _______, _______,         \
-  _______,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_UNDS, KC_EQL,  KC_BSLS, KC_SLSH, _______,         \
+  KC_TILD,        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS, _______,         \
+  KC_SPC,         _______, _______, _______, KC_PLUS, _______,  _______, KC_LBRC, KC_RBRC, KC_PLUS, _______, _______,         \
+  _______,        _______, _______, _______, KC_MINS, _______,  KC_UNDS, KC_SPC,  KC_BSPC, _______, _______, _______,         \
   _______,        _______, _______, _______, _______, KC_ESC,   KC_ESC,  _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY          \
 ),
 
 [_RAISE] = LAYOUT_planck_grid( \
-  KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,         \
-  KC_SPC,         _______, _______, _______, _______, _______,  KC_LT,   KC_LCBR, KC_RCBR, KC_GT,   _______, _______,         \
-  _______,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_MINS, KC_PLUS, KC_PIPE, KC_QUES, _______,         \
+  KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,         \
+  _______,        _______, _______, _______, _______, _______,  KC_PLUS, KC_LPRN, KC_RPRN, KC_EQL,  KC_COLN, _______,         \
+  _______,        _______, _______, _______, _______, _______,  KC_MINS, KC_LCBR, KC_RCBR, KC_PIPE, KC_BSLS, _______,         \
   _______,        _______, _______, _______, _______, KC_ESC,   KC_ESC,  _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY          \
 ),
 
